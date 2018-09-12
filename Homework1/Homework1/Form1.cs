8356a2a901cc498d31519fed3d7d4f7d3bf4984e
念麟 余 <yunianlin@outlook.com>
2018-09-12 18:32:05 +0800

    第一次作业

---
+++ Homework1/Homework1/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Homework1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string A = textBox1.Text;
+            string B = textBox2.Text;
+            double a;
+            double b;
+            double c;
+            a = double.Parse(A);
+            b = double.Parse(B);
+            c = a * b;
+            label3.Text = "" + c;
+        }
+    }
+}
---
+++ Homework1/topic4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace topic4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 0;
+            double b = 0;
+            double c = 0;
+            string s = " ";
+            Console.Write("Please input the first number: ");
+            s = Console.ReadLine();
+            a = double.Parse(s);
+            Console.Write("Please input the second number: ");
+            s = Console.ReadLine();
+            b = double.Parse(s);
+            c = a * b;
+            Console.WriteLine($"The result is: {c}.");
+        }
+    }
+}
